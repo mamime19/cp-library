9f26db89aa0bfdb4955cf6d6b9fb311901902416
mamime19 <tatsu25sun@gmail.com>
2023-07-09 11:00:00 +0900

    RMQ

---
+++ data_structures/RangeMinimumQuery.hpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template <typename T> class RMQ {
+  private:
+    int n;
+    vector<T> dat;
+
+  public:
+    RMQ(int n_) : n(), dat(n_ * 4, 2e9) {
+        int x = 1;
+        while (n_ > x) {
+            x *= 2;
+        }
+        n = x;
+    }
+    void update(int i, T x) {
+        i += n - 1;
+        dat[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;
+            dat[i] = min(dat[i * 2 + 1], dat[i * 2 + 2]);
+        }
+    }
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+    T query_sub(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) {
+            return 2e9;
+        } else if (a <= l && r <= b) {
+            return dat[k];
+        } else {
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return min(vl, vr);
+        }
+    }
+};
